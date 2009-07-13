fe53ee52b4ddc97759f25b824be7d3572e412eb0
chriswillarddesign <chriswillarddesign@9820d822-6bfb-11de-825f-edc1bca65f01>
2009-07-13 03:15:45 +0000

    First Commit - Basic Engine and Sprite Engine

---
+++ ataleofmagick.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+sprite.py
+
+Created by C on 2009-07-08.
+Copyright (c) 2009 2D-DeV. All rights reserved.
+"""
+
+import sys
+sys.path.insert(0, "lib")
+from game import Game
+
+"""
+Run app
+"""
+if __name__ == '__main__':
+	taleofmagick = Game()
+
